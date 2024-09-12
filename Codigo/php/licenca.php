d0c1f9152e7e06f2a038a83a3d4e3bee5284bade
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2024-09-12 10:35:36 -0300

    commit

--- Codigo/php/licenca.php
+++ Codigo/php/licenca.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="licenca.css">
+    <link rel="stylesheet" href="../css/licenca.css">
 </head>
 <body>
 
@@ -14,20 +14,38 @@
     <h1 align="center" id="lic-title1">BANCO DE DADOS HELLO WORLD!</h1>
     </div>
     <div class="lic-submain">
-        <div class="lic-">
-            <h1>PADRÃO</h1>
+        <div class="lic-div">
+            <h1 id="lic-planos">PADRÃO</h1>
             <br>
-            <h2>Acesso Básico ao Banco de Dados</h2>
+            <h2 id="sub-lic">Acesso Básico ao Banco de Dados</h2>
             <p>Informações Limitadas: Acesso a dados essenciais de clientes, reservas e feedback.</p>
             <br>
-            <h2>Interface de Usuário</h2>
+            <h2 id="sub-lic">Interface de Usuário</h2>
             <p>Painel de Controle Avançado: Interface completa com personalização e acesso a funcionalidades avançadas.</p>
             <p>Relatórios Detalhados: Relatórios personalizados e análises avançadas sobre desempenho, tendências e comportamento do cliente.</p>
             <br>
-            <h2>Interface de Usuário</h2>
+            <h2 id="sub-lic">Interface de Usuário</h2>
             <p>Suporte Prioritário: Atendimento ao cliente e suporte técnico prioritário via telefone e chat, 24/7.</p>
-        </div class="">
-            <h1>oi</h1>
+            <br>
+            <br>
+            <p id="price">Mensal: R$ 499</p>
+            <p id="price">Anual: R$ 5.499 (com dois meses gratuitos)</p>
+        </div>
+        <div class="lic-div lic-div1">
+            <h1 id="lic-planos">LICENÇA COMPLETA</h1>
+            <br>
+            <h2 id="sub-lic">Plano Padrão INCLUSO</h2>
+            <p id="mais">+</p>
+            <P>Classificação e Segmentação: Agrupamento de clientes com base em critérios como comportamento, histórico de compras e preferências.</p>
+            <br>
+            <p>Atualizações Diárias: Atualizações em tempo real para garantir que os dados estejam sempre atualizados.</p>
+            <br>
+            <h2 id="sub-lic">Integrações</h2>
+            <p>Integração Completa: Integração com uma ampla gama de sistemas de gestão, CRM (Customer Relationship Management)</p>
+            <br>
+            <p id="price">Mensal: R$ 499</p>
+            <p id="price">Anual: R$ 5.499 (com dois meses gratuitos)</p>
+        </div>
     </div>
     
 </div>
