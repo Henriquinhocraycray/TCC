fbc5ffee0bb66805e3fd20ca68121339f23dcba4
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-09-12 11:52:25 -0300

    \asdcv

--- Codigo/php/header.php
+++ Codigo/php/header.php
@@ -1,1 +1,31 @@
-<h1>oi</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" type="text/css" media='screen' href="../css/navbar.css">
+</head>
+<body>
+    <header>
+        <div class="background">
+            <div class="navbar">
+                <div>
+                    <img class="logo_img" src="" alt="">
+                </div>
+                <div class="op">
+                    <a class="" href="#">Página inicial</a>
+                </div>
+                <div class="op">
+                    <a class=""  href="#">Dados da empresa</a>
+                </div>
+                <div class="op">
+                    <a class="" href="#">Dados gerais</a>
+                </div>
+                    <div class="topic"></div>
+            </div>
+
+        </div>
+    </header>
+</body>
+</html>
