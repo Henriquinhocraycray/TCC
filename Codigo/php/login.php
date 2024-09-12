6db105ac408be5f0d79c0741c5aaa0087db2a022
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2024-09-12 11:52:36 -0300

    commit

--- Codigo/php/login.php
+++ Codigo/php/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login de Funcionário</title>
-    <link rel="stylesheet" href="../css/login1.css">
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
 <div class="container">
