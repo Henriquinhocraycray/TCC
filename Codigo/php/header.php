bcab7462bcc9058a2d6560a6067c2e26d4ce4bb0
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-09-20 09:22:50 -0300

    hgf

--- Codigo/php/header.php
+++ Codigo/php/header.php
@@ -3,31 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Hamburger Menu</title>
     <link rel="stylesheet" type="text/css" media='screen' href="../css/header.css">
 </head>
 <body>
     <header>
-        <div class="background">
-            <div class="navbar">
-                <div>
-                    <img class="logo_img" src="" alt="">
-                </div>
-                <div class="op">
-                    <a class="" href="#">Página inicial</a>
-                </div>
-                <div class="op">
-                    <a class=""  href="#">Dados da empresa</a>
-                </div>
-                <div class="op">
-                    <a class="" href="#">Dados gerais</a>
-                </div>
-                    <div class="topic">
-                        <img src="/imagens/Group 3.png" alt="" class="topic_img" href="#">
-                    </div>
+        <div class="navbar">
+            <div>
+                <img class="logo_img" src="" alt="">
             </div>
+            <div class="op">
+                <a href="#">Página inicial</a>
+            </div>
+            <div class="op">
+                <a href="#">Dados da empresa</a>
+            </div>
+            <div class="op">
+                <a href="#">Dados gerais</a>
+            </div>
+
+            <div class="off-screen-menu">
+                <div class="off-screen-items">
+                    <ul style="list-style: none;">
+                    
+                        <li>Termos de condições de uso</li>
+                        <li>Configurações da conta</li>
+                        <li>Sair</li>
+                    
+                    </ul>
+                </div>
+            </div>
+
+            <nav>
+                <div class="ham-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </nav>
 
         </div>
     </header>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const hamMenu = document.querySelector('.ham-menu');
+            const offScreenMenu = document.querySelector('.off-screen-menu');
+
+            hamMenu.addEventListener('click', () => {
+                hamMenu.classList.toggle('active');
+                offScreenMenu.classList.toggle('active');
+            });
+        });
+    </script>
 </body>
 </html>
