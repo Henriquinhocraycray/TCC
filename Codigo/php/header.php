9374f3a798a60a7f66e9de2cc7b02006caefd190
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-09-20 11:04:51 -0300

    xdfgb

--- Codigo/php/header.php
+++ Codigo/php/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hamburger Menu</title>
-    <link rel="stylesheet" type="text/css" media='screen' href="../css/header.css">
+    <link rel="stylesheet" type="text/css" media='screen' href="../css/header1.css">
 </head>
 <body>
     <header>
