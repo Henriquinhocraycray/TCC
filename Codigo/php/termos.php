819a93ae4b00750d6c7c25c9c08d5d87c554c690
José Luiz <130668873+zeluiz9571@users.noreply.github.com>
2024-09-20 13:27:23 -0300

    efrgrffvgbc nvhrgfhnvdtgre3f
    wegfrtnr2efgbe2fgrbe2rfgb

--- Codigo/php/termos.php
+++ Codigo/php/termos.php
@@ -19,8 +19,8 @@ $title = "Termos e Uso";
     
     <main>
         <section>
-            <h2>Data de Vigência: 06/12/2024</h2>
-            <p>Bem-vindo ao Serviço de Hospedagem de Informações da LaR Database (“Serviço”). Estes Termos e Condições de Uso (“Termos”) estabelecem as regras para acessar e utilizar o Serviço, que é destinado ao gerenciamento e armazenamento de dados de empregados e clientes de hotéis e pousadas. Ao utilizar o Serviço, você concorda em cumprir estes Termos. Se não concordar com qualquer parte dos Termos, você não deve usar o Serviço.</p>
+            <h2> Data de Vigência: 06/12/2024</h2>
+            <p> Bem-vindo ao Serviço de Hospedagem de Informações da LaR Database (“Serviço”). Estes Termos e Condições de Uso (“Termos”) estabelecem as regras para acessar e utilizar o Serviço, que é destinado ao gerenciamento e armazenamento de dados de empregados e clientes de hotéis e pousadas. Ao utilizar o Serviço, você concorda em cumprir estes Termos. Se não concordar com qualquer parte dos Termos, você não deve usar o Serviço.</p>
         </section>
 
         <section>
@@ -28,71 +28,88 @@ $title = "Termos e Uso";
             <h4>Aceitação e Concordância</h4>
             <p>Ao acessar ou utilizar o Serviço, você confirma que leu, compreendeu e concorda com todos os Termos. Estes Termos constituem um contrato vinculativo entre você e a LaR Database.</p>
             <h4>Se Não Concordar</h4>
-            <p>e você não concordar com qualquer parte destes Termos, deve interromper o uso do Serviço imediatamente. O uso continuado do Serviço após alterações a estes Termos será considerado como aceitação das mudanças.</p>
+            <p> Se você não concordar com qualquer parte destes Termos, deve interromper o uso do Serviço imediatamente. O uso continuado do Serviço após alterações a estes Termos será considerado como aceitação das mudanças.</p>
         </section>
 
         <section>
             <h2> 2. Modificações dos Termos</h2>
             <h4> Alterações dos Termos</h4>
-            <p>A LaR Database pode modificar estes Termos periodicamente para refletir mudanças nos serviços, atualizações tecnológicas ou para atender a requisitos legais. Alterações serão publicadas em nosso site ou aplicativo e entrarão em vigor na data de publicação.</p>
+            <p> A LaR Database pode modificar estes Termos periodicamente para refletir mudanças nos serviços, atualizações tecnológicas ou para atender a requisitos legais. Alterações serão publicadas em nosso site ou aplicativo e entrarão em vigor na data de publicação.</p>
             <h4> Como Acompanhar as Alterações</h4>
-            <p>É sua responsabilidade revisar regularmente estes Termos. A continuidade no uso do Serviço após a publicação de modificações indica sua aceitação dos Termos alterados.</p>
+            <p> É sua responsabilidade revisar regularmente estes Termos. A continuidade no uso do Serviço após a publicação de modificações indica sua aceitação dos Termos alterados.</p>
         </section>
 
         <section>
             <h2> 3. Uso do Serviço</h2>
             <h4> Regras de Uso</h4>
-            <p>Você concorda em utilizar o Serviço de forma legal e de acordo com estes Termos. É proibido:</p>
-            <h4>Abuso do Serviço</h4>
+            <p> Você concorda em utilizar o Serviço de forma legal e de acordo com estes Termos. É proibido:</p>
+            <h4> Abuso do Serviço</h4>
             <p> - Ataques de DDoS: Intencionalmente sobrecarregar nossos sistemas.</p>
             <p> - Distribuição de Malware: Espalhar software prejudicial que comprometa a segurança do Serviço.</p>
             <h4> Acesso Não Autorizado</h4>
             <p> - Hacking: Tentar acessar áreas restritas sem permissão.</p>
             <p> - Phishing: Coletar informações pessoais de maneira fraudulenta.</p>
             <h4> Conteúdo Proibido</h4>
-            <p>- Ilegalidade: Publicar ou distribuir qualquer material que viole leis ou regulamentos aplicáveis.</p>
-            <p>- Ofensa: Conteúdos que causem dano emocional ou desconforto a outros.- Difamação: Material que prejudique a reputação de indivíduos ou entidades.</p>
-            <p>- Obscenidade: Conteúdo sexualmente explícito ou que promova comportamentos indecentes.</p>
-            <p>- Violação de Propriedade Intelectual: Infringir direitos autorais, marcas registradas ou outros direitos de propriedade intelectual.</p>
+            <p> - Ilegalidade: Publicar ou distribuir qualquer material que viole leis ou regulamentos aplicáveis.</p>
+            <p> - Ofensa: Conteúdos que causem dano emocional ou desconforto a outros.- Difamação: Material que prejudique a reputação de indivíduos ou entidades.</p>
+            <p> - Obscenidade: Conteúdo sexualmente explícito ou que promova comportamentos indecentes.</p>
+            <p> - Violação de Propriedade Intelectual: Infringir direitos autorais, marcas registradas ou outros direitos de propriedade intelectual.</p>
         </section>
 
         <section>
-            <h2>4. Conta de Usuário</h2>
-            <h4>Criação e Gerenciamento da Conta</h4>
-            <p>Para utilizar certas funcionalidades do Serviço, você deve criar uma conta. Ao criar e gerenciar sua conta, você deve:</p>
-            <p>- Informações Precisos: Fornecer informações verdadeiras, completas e atualizadas.</p>
-            <p>- Segurança da Conta: Proteger suas credenciais de acesso e notificar imediatamente a LaR Database sobre qualquer uso não autorizado ou atividade suspeita</p>
+            <h2> 4. Conta de Usuário</h2>
+            <h4> Criação e Gerenciamento da Conta</h4>
+            <p> Para utilizar certas funcionalidades do Serviço, você deve criar uma conta. Ao criar e gerenciar sua conta, você deve:</p>
+            <p> - Informações Precisos: Fornecer informações verdadeiras, completas e atualizadas.</p>
+            <p> - Segurança da Conta: Proteger suas credenciais de acesso e notificar imediatamente a LaR Database sobre qualquer uso não autorizado ou atividade suspeita</p>
         </section>
 
         <section>
             <h2>5. Propriedade Intelectual</h2>
-            <h4>Direitos sobre o Conteúdo</h4>
-            <p>Todo o conteúdo e material disponível no Serviço, incluindo marcas registradas, logotipos, textos, gráficos, imagens e software, é propriedade da LaR Database ou de nossos licenciadores e está protegido por leis de propriedade intelectual.</p>
-            <p>- Reprodução e Modificação: É proibido copiar, modificar, distribuir ou criar obras derivadas do conteúdo sem autorização prévia.</p>
+            <h4> Direitos sobre o Conteúdo</h4>
+            <p> Todo o conteúdo e material disponível no Serviço, incluindo marcas registradas, logotipos, textos, gráficos, imagens e software, é propriedade da LaR Database ou de nossos licenciadores e está protegido por leis de propriedade intelectual.</p>
+            <p> - Reprodução e Modificação: É proibido copiar, modificar, distribuir ou criar obras derivadas do conteúdo sem autorização prévia.</p>
             <p>- Distribuição e Exibição: A distribuição ou exibição pública do conteúdo sem permissão é estritamente proibida.</p>
         </section>
 
         <section>
             <h2> 6. Proteção de Dados Pessoais</h2>
             <h4> Compromisso com a LGPD</h4>
-            <p>Estamos comprometidos em proteger suas informações pessoais de acordo com a Lei Geral de Proteção de Dados (Lei nº 13.709/2018 - LGPD). Nossa [Política de Privacidade] explica detalhadamente como coletamos, utilizamos, armazenamos e protegemos suas informações pessoais.</p>
+            <p> Estamos comprometidos em proteger suas informações pessoais de acordo com a Lei Geral de Proteção de Dados (Lei nº 13.709/2018 - LGPD). Nossa [Política de Privacidade] explica detalhadamente como coletamos, utilizamos, armazenamos e protegemos suas informações pessoais.</p>
             <h3> Coleta e Uso de Dados</h3>
             <h4> Coleta de Dados</h4>
-            <p>Coletamos informações necessárias para operar eficazmente o Serviço, incluindo:</p>
-            <p>- Dados Pessoais: Nome, endereço, e-mail, telefone e outras informações essenciais.</p>
-            <p>- Dados do Serviço: Informações sobre reservas, histórico de interações e preferências.</p>
-            <h4>Uso dos Dados</h4>
-            <h5>Utilizamos suas informações para:</h5>
-            <p>- Prestação e Melhoria do Serviço: Gerenciar e aprimorar as funcionalidades do Serviço.</p>
-            <p>- Comunicação: Enviar atualizações, responder a consultas e resolver problemas</p>
+            <p> Coletamos informações necessárias para operar eficazmente o Serviço, incluindo:</p>
+            <p> - Dados Pessoais: Nome, endereço, e-mail, telefone e outras informações essenciais.</p>
+            <p> - Dados do Serviço: Informações sobre reservas, histórico de interações e preferências.</p>
+            <h4> Uso dos Dados</h4>
+            <h5> Utilizamos suas informações para:</h5>
+            <p> - Prestação e Melhoria do Serviço: Gerenciar e aprimorar as funcionalidades do Serviço.</p>
+            <p> - Comunicação: Enviar atualizações, responder a consultas e resolver problemas</p>
             <h5> Armazenamento e Segurança</h5>
-            <p>Implementamos medidas rigorosas para proteger suas informações, incluindo:</p>
-            <p>- Criptografia: Proteção dos dados durante a transmissão e o armazenamento.</p>
-            <p>- Controle de Acesso: Restrição de acesso a dados sensíveis.</p>
-            <p>- Backup e Recuperação: Procedimentos para garantir a integridade dos dados e recuperação em caso de falhas.</p>
-            <h6>Apesar das nossas medidas de segurança, não podemos garantir proteção absoluta contra todos os riscos.</h6>
+            <p> Implementamos medidas rigorosas para proteger suas informações, incluindo:</p>
+            <p> - Criptografia: Proteção dos dados durante a transmissão e o armazenamento.</p>
+            <p> - Controle de Acesso: Restrição de acesso a dados sensíveis.</p>
+            <p> - Backup e Recuperação: Procedimentos para garantir a integridade dos dados e recuperação em caso de falhas.</p>
+            <h6> Apesar das nossas medidas de segurança, não podemos garantir proteção absoluta contra todos os riscos.</h6>
+        </section>
+
+        <section>
+            <h2> 7. Isenção de Responsabilidade</h2>
+            <h4> Limitações da Responsabilidade</h4>
+            <p> O Serviço é fornecido "no estado em que se encontra" e "conforme disponível". Não garantimos que o Serviço estará sempre disponível, seguro ou livre de erros.</p>
+            <p> - Danos Diretos e Indiretos: Na máxima extensão permitida pela lei, incluindo a LGPD, não somos responsáveis por danos indiretos, incidentais, especiais ou consequenciais resultantes do uso ou da incapacidade de usar o Serviço. Isso inclui, mas não se limita a, perda de dados e interrupção de negócios</p>
+        </section>
+
+        <section>
+            <h2>  8. Indenização</h2>
+            <h4> Responsabilidade por Danos</h4>
+            <p> Você concorda em indenizar e isentar a LaR Database, seus afiliados, diretores, funcionários e agentes de qualquer reclamação, dano, perda ou despesa (incluindo honorários advocatícios) resultante de:</p>
+            <p> - Uso Indevido: Qualquer violação destes Termos.</p>
+            <p> - Violação de Direitos: De terceiros, relacionada a conteúdo transmitido ou publicado por você.</p>
         </section>
     </main>
+    <br>
+    <hr>
 
     <footer>
         <p>&copy; <?php echo date("Y"); ?> LaR Database 2024. Todos os direitos reservados.</p>
