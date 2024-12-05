7cf4c68a0a1085f2fa081c2e3c22b3d5b2ca6231
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-12-05 15:03:03 -0300

    Update dados_gerais.php

--- Codigo/php/dados_gerais.php
+++ Codigo/php/dados_gerais.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Home</title>
     <link rel="stylesheet" type="text/css" media='screen' href="../css/dados_gerais.css">
 </head>
 <?php
