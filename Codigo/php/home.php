84f4ff55517f1a5978e8293200a7c34994e700c5
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-12-06 09:24:17 -0300

    zx

--- Codigo/php/home.php
+++ Codigo/php/home.php
@@ -8,7 +8,7 @@ $title = "home";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
+    <title>Home</title>
     <link rel="stylesheet" href="../css/home.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
